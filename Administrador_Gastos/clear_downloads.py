5a96457b0a898a1343a36f05fda965884959463a
FerSoriano <fer8f10@hotmail.com>
2023-05-27 15:39:20 -0600

    se crea carpeta con el proyecto v1

---
+++ Administrador_Gastos/clear_downloads.py
@@ -0,0 +1,48 @@
+# Programa para administrar la carpeta de descargas
+
+from pathlib import Path
+import shutil
+
+
+def clear_downloads():
+    descargas         = 'C:/Users/fer8f/Downloads'
+
+    folderAudios      =  descargas + '/Descargas_Audios'
+    folderEjecutables =  descargas + '/Descargas_Ejecutables'
+    folderExcel       =  descargas + '/Descargas_Excel'
+    folderImagenes    =  descargas + '/Descargas_Imagenes'
+    folderOtros       =  descargas + '/Descargas_Otros'
+    folderPDF         =  descargas + '/Descargas_PDF'
+
+    listExcel = ['.xls','.xlsx','.csv', '.xlsm']
+    listPDF = ['.pdf']
+    listEXE = ['.exe']
+    listMusic = ['.mp3','.mp4']
+    listPhoto = ['.jpg','.jpeg', '.png']
+
+    pathFiles = Path(descargas)
+
+    for e in pathFiles.iterdir():
+        suf = e.suffix
+        if suf == '':
+            continue
+        elif suf in listExcel:
+            shutil.move(e, folderExcel + '/' + e.name)
+        elif suf in listPDF:
+            shutil.move(e, folderPDF + '/' + e.name)
+        elif suf in listEXE:
+            shutil.move(e, folderEjecutables + '/' + e.name)
+        elif suf in listMusic:
+            shutil.move(e, folderAudios + '/' + e.name)
+        elif suf in listPhoto:
+            shutil.move(e, folderImagenes + '/' + e.name)
+        # else: 
+        #     try:
+        #         shutil.move(e, folderOtros + '/' + e.name)
+        #     except:
+        #         print('se encontro algun error.')
+                
+
+    print('Done. Se movieron los archivos.')
+
+
