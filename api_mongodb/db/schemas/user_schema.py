3bf5084512a7f6eb484f00cf902eafff970ad7b9
FerSoriano <fer8f10@hotmail.com>
2023-09-16 23:57:27 -0600

    Fase 1. Conexion a la BD. Metodos API Crear y Leer registros ya funcionan

---
+++ api_mongodb/db/schemas/user_schema.py
@@ -0,0 +1,30 @@
+def user_schema(user) -> dict:
+    """
+    Return a User class dict 
+    """
+    return {"id": str(user["_id"]),
+            "username": user["username"],
+            "email": user["email"]}
+
+def users_schema(users) -> list:
+    """
+    Return a User class list 
+    """
+    return [user_schema(user) for user in users]
+
+def user_meta_schema(user) -> dict:
+    """
+    Return a UserMetadata class dict 
+    """
+    return {"id": str(user["_id"]),
+            "username": user["username"],
+            "email": user["email"],
+            "created_at" : user["created_at"],
+            "last_modified": user["last_modified"],
+            "is_new": user["is_new"]}
+
+def users_meta_schema(users) -> list:
+    """
+    Return a UserMetadata class list 
+    """
+    return [user_meta_schema(user) for user in users]
