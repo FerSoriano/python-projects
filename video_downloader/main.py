d64bc76df47a8e3b9e6c146fd307bae89aa89fbf
fersoriano <fer8f10@hotmail.com>
2023-10-26 22:16:49 -0600

    Se agregan nuevo proyecto

---
+++ video_downloader/main.py
@@ -0,0 +1,33 @@
+
+import config
+import os
+
+EPISODIO = 'Ep-01'
+WORKBOOK = 'Futbol-Champagne'
+WORKSHEET = 'videos'
+CSV = 'videos.csv'
+PATH_VIDEOS = f'/Users/fersoriano/Documents/Proyectos/Podcast/videos/{EPISODIO}/downloads'
+
+
+texto = f"""
+--------------------------------
+*Episodio actual: {EPISODIO}*
+Esta correcto el Episodio? ('y' para continuar o cualquiera para salir): """
+
+
+r = input(texto).lower()
+
+if r == 'y':
+    conexion = config.GoogleConnection(service_account=config.get_service_account(),workbook=WORKBOOK,worksheet=WORKSHEET,csv=CSV)
+    conexion.setConection()
+    conexion.getRecords()
+    conexion.moveDataToCSV(EPISODIO)
+
+    videos = config.DescargaVideos(CSV)
+    videos.download(PATH_VIDEOS)
+
+    print('Proceso completado.')
+
+else:
+    os.system('clear')
+    exit()
