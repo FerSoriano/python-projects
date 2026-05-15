2551bbbc4f452c9ce90b2e51481744411562bee1
Fer Soriano <fer8f10@hotmail.com>
2026-05-14 22:20:27 -0600

    feat: add config.json and integrate GoogleCalendarManager for event creation

--- payment_calendar/main.py
+++ payment_calendar/main.py
@@ -1,5 +1,7 @@
 import datetime
-from modules import GoogleTaskManager, ReadGoogleSheet, EmailNotification
+from modules import GoogleTaskManager, ReadGoogleSheet, EmailNotification, GoogleCalendarManager
+import json
+
 
 WORKBOOK = 'Gastos Mensuales'
 WORKSHEET = 'Control Pagos'
@@ -7,33 +9,82 @@ HEADERS = ['Tipo', 'Concepto', 'Fecha', 'Fecha Pago', 'Semana', 'Monto', 'Coment
 LIST_NAME = 'Mis tareas'
 PAYMENT_TYPES = ['Tarjetas', 'Carro', 'Estudios', 'Mantenimiento']
 
-week_num = datetime.datetime.today().isocalendar()[1]
 
-googleSheetData = ReadGoogleSheet(workbook=WORKBOOK,worksheet=WORKSHEET,headers=HEADERS)
-googleTasks = GoogleTaskManager()
-email = EmailNotification()
+with open('config.json', 'r', encoding='utf-8') as f:
+    config = json.load(f)
 
-googleSheetData.setConection()
-df = googleSheetData.getRecords()
-try:
-    df = df[(df['Tipo'].isin(PAYMENT_TYPES)) & (df['Semana']==week_num)]
-except:
-    email.sendFailedNotification("Error al obtener el Dataframe.")
-    exit()
 
-body = ""
-for i,row in df.iterrows():
-    task_name = f'Pagar {row['Concepto']}'
-    due_date = row['Fecha Pago']
-    notes = f'Total a pagar: {row['Monto']}'
+def main():
+    week_num = datetime.datetime.today().isocalendar()[1]
+
+    google_sheet_data = ReadGoogleSheet(workbook=WORKBOOK,worksheet=WORKSHEET,headers=HEADERS)
+    email = EmailNotification()
+
+    google_tasks = None
+    google_calendar = None
+
+    if config['create_task']:
+        google_tasks = GoogleTaskManager()
+
+    if config['create_event']:
+        google_calendar = GoogleCalendarManager()
+
+    df = google_sheet_data.getRecords()
+
+    if df is None:
+        email.sendNotification(
+            subject="Payment Calendar: Dataframe vacio",
+            body="No pudo obtener informacion de Google Sheets."
+        )
+        return 
+
+    body = ""
     
-    create_task = googleTasks.createTask(task_list_name=LIST_NAME, task_name=task_name, due_date=due_date, notes=notes)
-    
-    if create_task[0]:
-        print(create_task[1])
-        body += create_task[1]
-        body += '\n'
-    else:
-        print(create_task[1])
+    try:
+        df = df[(df['Tipo'].isin(PAYMENT_TYPES)) & (df['Semana']==week_num)]
+    except:
+        email.sendFailedNotification(
+            subject="Payment Calendar: ERROR",
+            body="Error al obtener el Dataframe."
+        )
+        return 
 
-email.sendNotification(body=body)
+    for i,row in df.iterrows():
+        task_name = f'Pagar {row['Concepto']}'
+        due_date = row['Fecha Pago']
+        notes = f'Total a pagar: {row['Monto']}'
+        
+        if google_tasks is not None:
+            create_task = google_tasks.createTask(
+                task_list_name=LIST_NAME, 
+                task_name=task_name, 
+                due_date=due_date, 
+                notes=notes
+            )
+        
+            if create_task[0]:
+                print(create_task[1])
+                body += create_task[1]
+                body += '\n'
+            else:
+                print(create_task[1])
+        
+
+        if google_calendar is not None:
+            create_event = google_calendar.add_event(
+                event_name=task_name,
+                due_date=due_date,
+                notes=notes
+            )
+
+            if create_event[0]:
+                body += create_event[1]
+                body += '\n'
+                
+    email.sendNotification(body=body)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
